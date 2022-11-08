70c64c8a7c067759fc3db226f164239a46d8f02b
Cyril Blin <cyril.blin@epita.fr>
2022-11-08 19:02:00 +0100

    feat: parse request and make request structure

--- src/request.c
+++ src/request.c
@@ -1,9 +1,40 @@
 #include "request.h"
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 
-// suppose perfect request
-struct request *parse_request()
+static void request_free(struct request *request)
 {
+    free(request);
+}
+
+static void *request_err(struct request *request)
+{
+    request_free(request);
     return NULL;
+}
+// suppose perfect request
+struct request *parse_request(int socket)
+{
+    struct request *request = calloc(1, sizeof(struct request));
+    char buffer[BUFFER_SIZE];
+    ssize_t read_len = recv(socket, buffer, BUFFER_SIZE, 0);
+    char *meth = strtok(buffer, " ");
+    if (strcmp(meth, "GET") == 0)
+        request->method = GET;
+    else if (strcmp(meth, "HEAD") == 0)
+        request->method = HEAD;
+
+    request->target = strtok(NULL, " ");
+    char *version = strtok(NULL, "\r");
+    if (strcmp("HTTP/1.1", version) != 0)
+        return request_err(request);
+
+    char *host = strtok(NULL, " ");
+    if (strcmp(host, "\nHost:") == 0)
+        request->host = strtok(NULL, "\r");
+
+    return request;
 }
--- src/request.h
+++ src/request.h
@@ -1,8 +1,11 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#define BUFFER_SIZE 1024
+
 enum method_type
 {
+    UNDEFINED,
     GET,
     HEAD,
 };
@@ -10,10 +13,12 @@ enum method_type
 struct request
 {
     enum method_type method;
-    char *host;
     char *target;
+    char *host;
 };
 
-struct request *parse_request(void);
+// Returns a request structure
+// NULL if the HTPP version is incorrect (not 1.1)
+struct request *parse_request(int socket);
 
 #endif /* ! REQUEST_H */
