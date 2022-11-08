6aeaa6bdb6630b9ba0f66c1afed2ecebd2cea520
Cyril Blin <cyril.blin@epita.fr>
2022-11-08 23:37:12 +0100

    feat: error codes on answer

--- src/answer.c
+++ src/answer.c
@@ -2,29 +2,43 @@
 
 #include <stdlib.h>
 
+#include "config.h"
+
 static char *build_date(void)
 {
     return NULL;
 }
 
-struct answer *build_answer(struct request *request)
+static char *build_path(char *root_dir, char *file_path)
+{
+    //
+}
+
+struct answer *build_answer(struct request *request, struct vhost *host)
 {
     struct answer *answer = calloc(1, sizeof(struct request));
     answer->status_code = 200;
     if (request == NULL)
     {
         answer->status_code = 505;
-        return;
+        return answer;
     }
-    if (request->method == UNDEFINED)
-        answer->method = 405;
+    else if (request->target == NULL || request->host == NULL)
+        answer->status_code = 400;
+    else if (request->method == UNDEFINED)
+        answer->status_code = 405;
+
+    char *filepath = build_path(host->root_dir, request->target);
+    if (access(filepath, F_OK) == -1)
+        answer->status_code = 404;
+    else if (access(filepath, R_OK) == -1)
+        answer->status_code = 403;
     // write HTTP/1.1 status_code reason_phrase
     // Date: build_date()
     build_date();
     // Content-Length: stat(2)
     // Connectino: close
     // \r\n
-    return NULL;
 }
 
 void send_answer(struct request *request)
